81b7d9185f485dbdfcf891e34101dfc31b23f469
dwi312 <dwi31287no5@gmail.com>
2021-11-13 22:01:40 +0700

    Belajar dan membahas mengenai konsep dasar dari MVC ini, dan.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- 
+        MODEL =
+     1. Respentasi pengetahuan
+     2. Mengolah data
+     3. Logika bisnis
+
+        VIEW = 
+     1. Output, apa yang dilihat oleh user
+     2. Respentasi visual dari model
+     3. Lapisan presentasi
+
+        CONTROLLER =
+     1. Perantara antara model dan view
+     2. Menangani pemrosesan pada aplikasi
+     3. Menangani aksi dari user
+
+
+     Aplication flow yang akan dibuat
+    
+    |           | -> | Routing | -> |                       |  -> |        |
+    | index.php |                   | Aplication Controller |     | Models | 
+    |           | <- |  View   | <- |                       |  <- |        |
+
+                                                                  
+ -->
